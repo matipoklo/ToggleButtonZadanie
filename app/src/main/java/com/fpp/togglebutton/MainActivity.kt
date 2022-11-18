59cdb6a3dc5788721fc60a096607dfaf3289e325
Filippo0420 <fpawelec0@gmail.com>
2022-11-18 15:07:12 +0100

    Dodano tablice przycisków

--- app/src/main/java/com/fpp/togglebutton/MainActivity.kt
+++ app/src/main/java/com/fpp/togglebutton/MainActivity.kt
@@ -2,10 +2,22 @@ package com.fpp.togglebutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttons = arrayOf(
+            findViewById<ToggleButton>(R.id.toggleButton1),
+            findViewById<ToggleButton>(R.id.toggleButton2),
+            findViewById<ToggleButton>(R.id.toggleButton3),
+            findViewById<ToggleButton>(R.id.toggleButton4),
+            findViewById<ToggleButton>(R.id.toggleButton5),
+            findViewById<ToggleButton>(R.id.toggleButton6)
+        )
+        
+
     }
 }
