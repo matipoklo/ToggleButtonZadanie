fc215f5df393dd36a5fa0f906eafc89f188a0589
Filippo0420 <fpawelec0@gmail.com>
2022-11-18 16:00:03 +0100

    Modyfikacja kodu

--- app/src/main/java/com/fpp/togglebutton/MainActivity.kt
+++ app/src/main/java/com/fpp/togglebutton/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fpp.togglebutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import android.widget.ToggleButton
 
@@ -17,7 +18,29 @@ class MainActivity : AppCompatActivity() {
             findViewById<ToggleButton>(R.id.toggleButton5),
             findViewById<ToggleButton>(R.id.toggleButton6)
         )
-        
-
+        val klucz = intArrayOf(1,2,3,4,5,6);
+        var numerDoKlucza = intArrayOf();
+        buttons[0].setOnClickListener(){
+            numerDoKlucza[0] = 1;
+        }
+        buttons[1].setOnClickListener(){
+            numerDoKlucza[1] = 2;
+        }
+        buttons[2].setOnClickListener(){
+            numerDoKlucza[2] = 3;
+        }
+        buttons[3].setOnClickListener(){
+            numerDoKlucza[3] = 4;
+        }
+        buttons[4].setOnClickListener(){
+            numerDoKlucza[4] = 5;
+        }
+        buttons[5].setOnClickListener(){
+            numerDoKlucza[5] = 6;
+        }
+        //1 3 2 6 4 5
+        if(numerDoKlucza == klucz){
+            findViewById<TextView>(R.id.txtWynik).text = "Wynik: 1"
+        }
     }
 }
